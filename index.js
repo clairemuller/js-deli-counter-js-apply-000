a94693872540051301c16679f7a8face470fbaed
Claire Muller <clairermuller@gmail.com>
2018-12-27 16:12:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function takeANumber(line){
+  let ticketNumber = 1;
+}
+
+takeANumber(line) // "Welcome! You're ticket number #1" [1]
+takeANumber(line) // "Welcome! You're ticket number #2" [1,2]
+
+nowServing(line) // "Currently serving #1" [2]
+nowServing(line) // "Currently serving #2" []
+
+takeANumber(line) // "Welcome! You're ticket number #3" [3]
